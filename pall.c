f835f64a2617384b7a9640c92e70fff5483d619c
underdogs180 <asadirshad38@gmail.com>
2021-09-25 02:10:31 -0700

    Modified

--- pall.c
+++ pall.c
@@ -1,26 +1,13 @@
 #include "monty.h"
 
 /**
- * m_pall - print all values on `stack' starting from the top
- * @stack: double pointer to head of stack
- * @line_number: line number being executed from script file
+ * pall - prints the stack
+ * @stack: stack given by main
+ * @line_cnt: ammount of lines
  *
  * Return: void
  */
-void m_pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
 {
-  stack_t *head;
-
-  (void)(line_number);
-
-  head = *stack;
-  while (head != NULL)
-    {
-      printf("%d\n", head->n);
-      head = head->next;
-      if (head == *stack)
-	{
-	  return;
-	}
-    }
+  print_stack(*stack);
 }
