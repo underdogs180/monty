99f6efdef1afa16544311ab5fedabbcf35ff684c
underdogs180 <asadirshad38@gmail.com>
2021-09-25 01:59:20 -0700

    Fixed betty errors

--- push.c
+++ push.c
@@ -13,29 +13,29 @@
  */
 void push(stack_t **stack, unsigned int line_cnt)
 {
-  char *n = global.argument;
+char *n = global.argument;
 
-  if ((is_digit(n)) == 0)
-    {
-      fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
-      status = EXIT_FAILURE;
-      return;
-    }
+if ((is_digit(n)) == 0)
+{
+fprintf(stderr, "L%d: usage: push integer\n", line_cnt);
+status = EXIT_FAILURE;
+return;
+}
 
-  if (global.data_struct == 1)
-    {
-      if (!add_node(stack, atoi(global.argument)))
-	{
-	  return;
-	  status = EXIT_FAILURE;
-	}
-    }
-  else
-    {
-      if (!queue_node(stack, atoi(global.argument)))
-	{
-	  return;
-	  status = EXIT_FAILURE;
-	}
-    }
+if (global.data_struct == 1)
+{
+if (!add_node(stack, atoi(global.argument)))
+{
+return;
+status = EXIT_FAILURE;
+}
+}
+else
+{
+if (!queue_node(stack, atoi(global.argument)))
+{
+return;
+status = EXIT_FAILURE;
+}
+}
 }
